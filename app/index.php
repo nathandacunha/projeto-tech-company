450eae79216f4103990e45fb9c5049a88596e0f9
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-12-30 19:16:00 -0300

    add: adição de novos arquivos php, como o index e o cadastro.

---
+++ app/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tech Company | Inicio</title>
+</head>
+<body>
+    <h1>boas vindas ao tech company</h1>
+    <a href="/projeto-tech-company/app/pages/login.php">Já tem conta?</a>
+    <a href="/projeto-tech-company/app/pages/cadastro.php"></a>
+</body>
+</html>
