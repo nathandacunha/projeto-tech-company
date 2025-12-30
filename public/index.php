dd4b8fa259b6e365f5d9b0e8606cc21c0fe0fa40
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-12-30 19:21:59 -0300

    feat: adição de css na pagina principal

--- public/index.php
+++ public/index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/projeto-tech-company/public/style/style.css">
     <title>Tech Company | Inicio</title>
 </head>
 <body>
     <h1>boas vindas ao tech company</h1>
-    <a href="/projeto-tech-company/app/pages/login.php">Já tem conta?</a>
-    <a href="/projeto-tech-company/app/pages/cadastro.php"></a>
+    <a href="/projeto-tech-company/src/View/login.php">Já tem conta?</a>
+    <a href="/projeto-tech-company/src/View/cadastro.php">Não tem conta?</a>
 </body>
 </html>
