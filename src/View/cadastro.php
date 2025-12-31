1710cfbb442c4758d4a6337254ae1eb7730e43f1
Nathan da Cunha <nathan_cunha@estudante.sesisenai.org.br>
2025-12-31 15:54:18 -0300

    feat(cadastro): seção navbar para mobile feita e a primeira parte do formulário de cadastro feito

---
+++ src/View/cadastro.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Cadastro</title>
+</head>
+<body>
+    <section id = "navbar_mobile">
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Tech Company</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    </section>
+    
+    <section>
+        <main>
+            <h1>Bem-vindo(a) ao cadastro</h1>
+
+            <form method="post" autocomplete="off">
+                <label>CPF: </label>
+                <input type="text" name="campo-cpf" id="campo-cpf" placeholder="CPF: " required>
+                <label>E-mail: </label>
+                <input type="email" name="campo-email" id="campo-email" placeholder="E-mail: " required>
+                <label>Senha: </label>
+                <input type="password" name="campo-senha" id="campo-senha" placeholder="Senha: " required>
+
+                <button><a href="#">Continuar</a></button>
+            </form>
+        </main>
+    </section>
+</body>
+</html>
